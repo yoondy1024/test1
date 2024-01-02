7ed241af1edb9962f4f5fea9d1c09f4547879612
yoondy1024 <155443428+yoondy1024@users.noreply.github.com>
2024-01-03 00:25:06 +0900

    sdsd

--- extension1/popup.js
+++ extension1/popup.js
@@ -1,9 +1,16 @@
-function setTimer() {
-    const minutes = document.getElementById('minutes').value;
-    const timeInMillis = minutes * 60;
+document.addEventListener('DOMContentLoaded', function() {
+    const setTimerButton = document.getElementById('setTimer');
+    setTimerButton.addEventListener('click', function() {
+      setTimer();
+    });
+  });
   
-    chrome.storage.sync.set({ timer: timeInMillis }, function() {
-      console.log('Timer is set to ' + timeInMillis + ' milliseconds');
+  function setTimer() {
+    const minutes = document.getElementById('minutes').value;
+    const timeInSeconds = minutes * 60;
+  
+    chrome.storage.sync.set({ timer: timeInSeconds }, function() {
+      console.log('Timer is set to ' + timeInSeconds + ' seconds');
       window.close();
     });
   }
