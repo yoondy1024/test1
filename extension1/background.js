82cdec9aca8ee7add1936532cf3df5256c686d91
yoondy1024 <155443428+yoondy1024@users.noreply.github.com>
2024-01-03 00:19:33 +0900

    ㅇㄹㅇㄹ

--- extension1/background.js
+++ extension1/background.js
@@ -1,11 +1,25 @@
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    chrome.storage.sync.get(['timer'], function(result) {
-      const timer = result.timer || 1 * 60 * 1000; // 기본값은 1분
-  
-      setInterval(() => {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-          chrome.tabs.remove(tabs[0].id);
-        });
-      }, timer);
-    });
+let timer = 0;
+let intervalId = null;
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.storage.sync.get(['timer'], function(result) {
+    const timeInSeconds = result.timer || 60; // 기본값은 1분
+
+    timer = timeInSeconds;
+    startCountdown(tab.id);
   });
+});
+
+function startCountdown(tabId) {
+  intervalId = setInterval(() => {
+    timer--;
+
+    if (timer >= 0) {
+      chrome.action.setBadgeText({ text: timer.toString() });
+    } else {
+      clearInterval(intervalId);
+      chrome.action.setBadgeText({ text: '' });
+      chrome.tabs.remove(tabId);
+    }
+  }, 1000);
+}
--- extension1/popup.js
+++ extension1/popup.js
@@ -1,6 +1,6 @@
 function setTimer() {
     const minutes = document.getElementById('minutes').value;
-    const timeInMillis = minutes * 60 * 1000;
+    const timeInMillis = minutes * 60;
   
     chrome.storage.sync.set({ timer: timeInMillis }, function() {
       console.log('Timer is set to ' + timeInMillis + ' milliseconds');
