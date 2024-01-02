c805a282739cf167fba2f404a36733afc86f6269
yoondy1024 <155443428+yoondy1024@users.noreply.github.com>
2024-01-03 00:29:18 +0900

    ㅇㄹㅇㄹ

--- extension1/popup.js
+++ extension1/popup.js
@@ -9,8 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const minutes = document.getElementById('minutes').value;
     const timeInSeconds = minutes * 60;
   
-    chrome.storage.sync.set({ timer: timeInSeconds }, function() {
-      console.log('Timer is set to ' + timeInSeconds + ' seconds');
-      window.close();
+    chrome.storage.sync.set({ 'timer': timeInSeconds }, function() {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      } else {
+        console.log('Timer is set to ' + timeInSeconds + ' seconds');
+        window.close();
+      }
     });
   }
